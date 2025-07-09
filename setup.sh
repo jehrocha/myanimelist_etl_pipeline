ade83a5f74b750de028ef6d383471266d7465cf8
Jan Edward Rocha <rainbow.purine-6k@icloud.com>
2025-07-09 12:53:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 export AIRFLOW_VERSION=3.0.2
 export PYTHON_VERSION=3.12
 
+# Export AIRFLOW_UID
+echo -e "AIRFLOW_UID=$(id -u)" > .env
+
 # Download the matching constraints file
 curl -sSL "https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt" -o constraints.txt
 
